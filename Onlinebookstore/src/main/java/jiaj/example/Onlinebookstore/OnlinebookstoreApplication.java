492fb940c2a6dc4cb353876e89ef6ad0180d623a
Janeliu123 <jiajliu6666@gmail.com>
2020-03-04 10:06:30 -0600

    Init.

---
+++ Onlinebookstore/src/main/java/jiaj/example/Onlinebookstore/OnlinebookstoreApplication.java
@@ -0,0 +1,13 @@
+package jiaj.example.Onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebookstoreApplication.class, args);
+	}
+
+}
---
+++ Onlinebookstore/src/test/java/jiaj/example/Onlinebookstore/OnlinebookstoreApplicationTests.java
@@ -0,0 +1,16 @@
+package jiaj.example.Onlinebookstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlinebookstoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
